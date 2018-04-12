227593fff41879c64a27692cbd38ed673cdc9666
manchovi <winmanuel07@gmail.com>
2018-04-12 14:15:30 -0600

    primer version de mi proyecto

---
+++ clases/crud.java
@@ -0,0 +1,19 @@
+public class crud{
+	
+	public funcion guardar(String campos, String valores, String Tabla){
+
+	}
+
+	public fuction actualizar(String id, String campos_valores, String Tabla){
+
+	}
+
+	public fuction consultar(String tabla){
+
+	}
+
+	public function eliminar(String id, String tabla){
+
+	}
+
+	}
